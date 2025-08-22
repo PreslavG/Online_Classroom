d420d1d3fbc095c0ac77b5c8f013ac09aebcf3a1
PreslavG <138732628+PreslavG@users.noreply.github.com>
2025-08-22 15:01:24 +0300

    Authentication

--- main.js
+++ main.js
@@ -3,14 +3,10 @@ import './style.css';
 import { db } from "./firebase";
 import { collection, addDoc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 
-// Initialize Firebase
+
+// Initialize Firebas
 
 
 const servers = {
