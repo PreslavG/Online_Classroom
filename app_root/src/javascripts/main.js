5efc9b6b57163210ce6b3ea8728790d18c0a11ee
PreslavG <138732628+PreslavG@users.noreply.github.com>
2025-08-23 18:10:23 +0300

    Vite installed

---
+++ app_root/src/javascripts/main.js
@@ -0,0 +1,65 @@
+import '../style.css';
+import { db } from "./firebase";
+import { collection, addDoc } from "firebase/firestore";
+
+
+const servers = {
+  iceServers: [
+    {
+      urls: [
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302'
+      ]
+    }
+  ]
+};
+
+// Global State
+const pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+// HTML elements
+const webcamButton = document.getElementById('webcamButton');
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
+
+// 1. Setup media sources
+
+webcamButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true});
+  remoteStream = new MediaStream();
+
+  // Push tracks from local stream to peer connection
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+
+  // Pull tracks from remote stream, add to video stream
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  webcamVideo.srcObject = localStream;
+
+
+  webcamButton.disabled = false;
+
+  const userId = null;
+  try {
+    await addDoc(collection(db, "myCollection"), {
+      userId: user,
+      createdAt: new Date()
+    });
+    console.log("Document saved with userId:", userId);
+  } catch (error) {
+    console.error("Error adding document:", error);
+  }
+};
+
